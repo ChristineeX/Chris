f72ea0f51ebc9e09034da5d71445f660bacf0b9f
Christinee_Xu <47691786+ChristineeX@users.noreply.github.com>
2019-03-03 22:20:48 +1100

    Update count.py

--- count.py
+++ count.py
@@ -1,19 +1,21 @@
 
-
+# e.g: text = HelloOo
 def count_char(text):
-    # TODO count the number of times each character occurs in the text
+	character = {}
+# the dictionary character: {"H":1, "e":1,"l":2, "o" :2..etc}
+# ch:represet the character, character[ch] represent the value
     # and print out each character along with its count
-    pass
+	for ch in text:
+		character[ch] = text.count(ch)	# count occurrances of each character
+	for i in character:
+		print(i + " " + str(character[i]))	
+	# convert int-type as string to allow printed
+		
 
 def count_char_insensitive(text):
-    # TODO do the same as `count_char` but in a case-insensitive manner
-    pass
-
+    
+	# convert all letters in text to lower case, run the count function
+	return count_char(text.lower())	
 
 def count_char_ordered(text):
-    # TODO print the characters in the descending order of the count
-    # HINT: lookup `sorted()` in the Python documentation
-    
-    # This task is quite difficult, so please feel free to make use of
-    # resources online (Python docs, Stack Overflow, etc.)
     pass
